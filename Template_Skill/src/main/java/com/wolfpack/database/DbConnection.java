882e9005efc7dc313710af195aa3cefd20f30261
erich <bucher.erich@gmail.com>
2016-09-29 13:30:19 -0700

    Add driver to gradle, removed comments about drivers

--- Template_Skill/src/main/java/com/wolfpack/database/DbConnection.java
+++ Template_Skill/src/main/java/com/wolfpack/database/DbConnection.java
@@ -1,9 +1,8 @@
 /**
  * Before attempting to use this class, follow the directions on the wiki at
  * https://github.com/370-Alexa-Project/CS370_Echo_Demo/wiki/Using-the-dbConnection-class
- * to make sure you have everything set up properly. Unfortunately, you must download
- * a driver (which can't go up on github) and you need to create a file to store your
- * username and password for the database (which you wouldn't want to put on Github either).
+ * to make sure you have everything set up properly. Examples of how to use this class
+ * are also provided.
  * 
  * This class can be used to simplify the process of connecting to a postgresql database.
  * In order to connect to the database, you should call getCredentials() before calling
