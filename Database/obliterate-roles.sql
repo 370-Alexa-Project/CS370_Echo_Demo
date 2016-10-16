8c2af56a8b2e9705dfe1fed260928eaef9c09743
Ryan Moeller <ryan@freqlabs.com>
2016-10-15 22:48:31 -0700

    Revert out-of-order commit
    This reverts commit 7561bf2f0c6520ca5508cdc23a2c05b724a260dc:
    "Break roles out of main files, adjust search path"

--- Database/obliterate-roles.sql
+++
@@ -1,9 +0,0 @@
--- BE CAREFUL! --
-
-REVOKE ALL ON DATABASE ssunews FROM alexaskill;
-REVOKE ALL ON DATABASE ssunews FROM scraper;
-REVOKE ALL ON DATABASE ssunews FROM ssuadmin;
-
-DROP ROLE alexaskill;
-DROP ROLE scraper;
-DROP ROLE ssuadmin;
--- Database/obliterate.sql
+++ Database/obliterate.sql
@@ -1,3 +1,11 @@
--- This file will wipe out the schema! BE CAREFUL! --
+-- This file will wipe out the database! BE CAREFUL! --
+
+REVOKE ALL ON DATABASE ssunews FROM alexaskill;
+REVOKE ALL ON DATABASE ssunews FROM scraper;
+REVOKE ALL ON DATABASE ssunews FROM ssuadmin;
 
 DROP SCHEMA ssucalendar CASCADE;
+
+DROP ROLE alexaskill;
+DROP ROLE scraper;
+DROP ROLE ssuadmin;
